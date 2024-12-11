82d71c94bb0d0e4d306d78b5f28670125c977dbc
Carson Myers <carson@myers.se>
2024-12-11 22:49:07 +0100

    wip day 5

--- src/challenge/day4.rs
+++ src/challenge/day4.rs
@@ -1,6 +1,12 @@
-use crate::challenge::Solver;
-use crate::input::Input;
+use std::collections::HashMap;
+use std::fmt::Display;
 use std::sync::{Arc, Mutex};
+
+use itertools::Itertools;
+
+use crate::challenge::Solver;
+use crate::input::helpers::InputHelper;
+use crate::input::Input;
 
 #[derive(Debug)]
 pub struct Day4 {
@@ -16,10 +22,234 @@ impl Solver for Day4 {
     }
 
     fn solve_part_1(&self) -> crate::challenge::Result<String> {
-        todo!()
+        let helper = InputHelper::new(4, self.input.clone());
+        let search = WordSearch::new(helper.all_text()?);
+
+        let x = search.find_all('X');
+        let m = x
+            .into_iter()
+            .flat_map(|pos| search.next_position(pos, 'M'))
+            .collect_vec();
+        let a = m
+            .into_iter()
+            .flat_map(|pos| search.next_position(pos, 'A'))
+            .collect_vec();
+        let s = a
+            .into_iter()
+            .flat_map(|pos| search.next_position(pos, 'S'))
+            .collect_vec();
+
+        Ok(s.len().to_string())
     }
 
     fn solve_part_2(&self) -> crate::challenge::Result<String> {
-        todo!()
+        use Direction::*;
+
+        let helper = InputHelper::new(4, self.input.clone());
+        let search = WordSearch::new(helper.all_text()?);
+
+        let m = search.find_all('M');
+        let a = m
+            .into_iter()
+            .flat_map(|pos| search.next_position(pos, 'A'))
+            .collect_vec();
+        let s = a
+            .into_iter()
+            .flat_map(|pos| search.next_position(pos, 'S'))
+            .filter(|mas| matches!(mas.direction, NE | NW | SE | SW))
+            .collect_vec();
+
+        let mut pos_map: HashMap<(usize, usize), Vec<Position>> = HashMap::new();
+        for pos in &s {
+            pos_map
+                .entry((pos.x, pos.y))
+                .and_modify(|e| e.push(*pos))
+                .or_insert_with(|| vec![*pos]);
+        }
+
+        let xmases = s
+            .iter()
+            .cloned()
+            .filter(|pos| {
+                match pos.direction {
+                    SE => vec![(SW, (-2, 0)), (NE, (0, -2))],
+                    SW => vec![(SE, (2, 0)), (NW, (0, -2))],
+                    NE => vec![(SE, (0, 2)), (NW, (-2, 0))],
+                    NW => vec![(SW, (0, 2)), (NE, (2, 0))],
+                    _ => unreachable!(),
+                }
+                .into_iter()
+                .filter_map(|(direction, (x, y))| {
+                    search
+                        .add_coords(pos, x, y)
+                        .map(|(x, y)| (direction, (x, y)))
+                })
+                .any(|(direction, (x, y))| {
+                    pos_map.get(&(x, y)).is_some_and(|other_positions| {
+                        other_positions
+                            .iter()
+                            .any(|other| other.direction == direction)
+                    })
+                })
+            })
+            .collect_vec();
+
+        Ok((xmases.len() / 2).to_string())
+    }
+}
+
+#[derive(Debug, Copy, Clone, Hash)]
+struct Position {
+    x: usize,
+    y: usize,
+    direction: Direction,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Direction {
+    Any,
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+    NW,
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Self::Any => "??",
+            Self::N => "N ",
+            Self::NE => "NE",
+            Self::E => " E",
+            Self::SE => "SE",
+            Self::S => "S ",
+            Self::SW => "SW",
+            Self::W => " W",
+            Self::NW => "NW",
+        };
+
+        write!(f, "{}", str)
+    }
+}
+
+struct WordSearch {
+    rows: usize,
+    cols: usize,
+    data: Vec<Vec<char>>,
+}
+
+impl WordSearch {
+    fn new<S: AsRef<str>>(input: S) -> Self {
+        let data = input
+            .as_ref()
+            .lines()
+            .map(|line| line.chars().collect_vec())
+            .collect_vec();
+
+        let rows = data.len();
+        let cols = data.first().map(Vec::len).expect("at least one row");
+
+        Self { rows, cols, data }
+    }
+
+    fn at(&self, x: usize, y: usize) -> Option<char> {
+        self.data.get(y)?.get(x).cloned()
+    }
+
+    fn find_all(&self, find_char: char) -> Vec<Position> {
+        self.data
+            .iter()
+            .enumerate()
+            .flat_map(|(y, cols)| {
+                cols.iter().enumerate().filter_map(move |(x, c)| {
+                    if *c == find_char {
+                        Some((x, y))
+                    } else {
+                        None
+                    }
+                })
+            })
+            .map(|(x, y)| Position {
+                x,
+                y,
+                direction: Direction::Any,
+            })
+            .collect_vec()
+    }
+
+    fn next_position(&self, pos: Position, find_char: char) -> Vec<Position> {
+        self.next_coords(&pos)
+            .into_iter()
+            .filter(|(_, (x, y))| self.at(*x, *y).is_some_and(|c| c == find_char))
+            .map(|(direction, (x, y))| Position { x, y, direction })
+            .collect_vec()
+    }
+
+    fn next_coords(&self, pos: &Position) -> Vec<(Direction, (usize, usize))> {
+        vec![
+            (Direction::N, self.add_coords(pos, 0, -1)),
+            (Direction::NE, self.add_coords(pos, 1, -1)),
+            (Direction::E, self.add_coords(pos, 1, 0)),
+            (Direction::SE, self.add_coords(pos, 1, 1)),
+            (Direction::S, self.add_coords(pos, 0, 1)),
+            (Direction::SW, self.add_coords(pos, -1, 1)),
+            (Direction::W, self.add_coords(pos, -1, 0)),
+            (Direction::NW, self.add_coords(pos, -1, -1)),
+        ]
+        .into_iter()
+        .filter_map(|(direction, coords)| coords.map(|coords| (direction, coords)))
+        .filter(|(direction, _)| match pos.direction {
+            Direction::Any => true,
+            _ => *direction == pos.direction,
+        })
+        .collect_vec()
+    }
+
+    fn add_coords(&self, pos: &Position, add_x: isize, add_y: isize) -> Option<(usize, usize)> {
+        let x = match pos.x as isize + add_x {
+            -1 => None,
+            n if n >= self.cols as isize => None,
+            n => Some(n as usize),
+        };
+
+        let y = match pos.y as isize + add_y {
+            -1 => None,
+            n if n >= self.rows as isize => None,
+            n => Some(n as usize),
+        };
+
+        x.and_then(|x| y.and_then(|y| Some((x, y))))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::input::helpers::test_input;
+
+    #[test]
+    fn test_solve() {
+        let input = test_input(
+            r#"
+            MMMSXXMASM
+            MSAMXMSMSA
+            AMXSXMAAMM
+            MSAMASMSMX
+            XMASAMXAMM
+            XXAMMXXAMA
+            SMSMSASXSS
+            SAXAMASAAA
+            MAMMMXMMMM
+            MXMXAXMASX
+        "#,
+        );
+
+        let solver = Day4::new(Arc::new(Mutex::new(input)));
+        assert_eq!(solver.solve_part_1().unwrap(), "18");
+        assert_eq!(solver.solve_part_2().unwrap(), "9");
     }
 }
---
+++ src/challenge/day5.rs
@@ -0,0 +1,178 @@
+use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, Mutex};
+
+use winnow::prelude::*;
+
+use crate::challenge::Solver;
+use crate::challenge::{Error, Result};
+use crate::input::helpers::InputHelper;
+use crate::input::Input;
+
+#[derive(Debug)]
+pub struct Day5 {
+    input: Arc<Mutex<dyn Input>>,
+}
+
+type Rules = HashMap<usize, HashSet<usize>>;
+type Updates = Vec<Vec<usize>>;
+
+impl Day5 {
+    fn validate_update(update: &[usize], rules: &Rules) -> bool {
+        let mut seen: HashSet<usize> = HashSet::new();
+
+        println!("update: {:?}", update);
+        for page in update {
+            let Some(page_rules) = rules.get(page) else {
+                println!("\t{}: no rule", page);
+                seen.insert(*page);
+                continue;
+            };
+
+            for cannot_precede in page_rules {
+                if seen.contains(cannot_precede) {
+                    println!("\t{}: page {} already seen", page, cannot_precede);
+                    return false;
+                } else {
+                    println!("\t{}: page {} has not been seen", page, cannot_precede);
+                }
+            }
+
+            println!("\t{}: rule {:?} passed", page, page_rules);
+            seen.insert(*page);
+        }
+
+        println!("\tPASS");
+        true
+    }
+
+    fn read_rules_and_updates(&self) -> Result<(Rules, Updates)> {
+        let helper = InputHelper::new(5, self.input.clone());
+        let data = self
+            .parse_rules_and_updates(&mut helper.all_text()?.as_str())
+            .map_err(|err| Error::LineParseError(err.to_string()))?;
+
+        Ok(data)
+    }
+
+    fn parse_rules_and_updates(&self, input: &mut &str) -> PResult<(Rules, Updates)> {
+        let (rules_lines, updates_lines) = self.parse_lines(input)?;
+
+        let mut rules = HashMap::new();
+        for rule in rules_lines {
+            rules
+                .entry(rule.0)
+                .and_modify(|e: &mut HashSet<usize>| {
+                    e.insert(rule.1);
+                })
+                .or_insert_with(|| {
+                    let mut set = HashSet::new();
+                    set.insert(rule.1);
+                    set
+                });
+        }
+        dbg!(&rules);
+
+        let mut updates = Vec::new();
+        for update in updates_lines {
+            updates.push(update);
+        }
+        dbg!(&updates);
+
+        Ok((rules, updates))
+    }
+    fn parse_lines(&self, input: &mut &str) -> PResult<(Vec<(usize, usize)>, Vec<Vec<usize>>)> {
+        use winnow::ascii::{dec_uint, line_ending};
+        use winnow::combinator::{repeat, separated, separated_pair, terminated};
+        use winnow::error::StrContext;
+
+        (
+            terminated(
+                separated(1.., separated_pair(dec_uint, "|", dec_uint), line_ending),
+                (line_ending, line_ending),
+            )
+            .context(StrContext::Label("rules section")),
+            separated(
+                1..,
+                    separated(
+                        1..,
+                        dec_uint::<_, usize, _>.context(StrContext::Label("update page")),
+                        ",",
+                    )
+                    .context(StrContext::Label("update")),
+                    line_ending,
+                )
+                .context(StrContext::Label("update line")),
+        )
+            .parse_next(input)
+    }
+}
+
+impl Solver for Day5 {
+    fn new(input: Arc<Mutex<dyn Input>>) -> Self
+    where
+        Self: Sized,
+    {
+        Self { input }
+    }
+
+    fn solve_part_1(&self) -> Result<String> {
+        let (rules, updates) = self.read_rules_and_updates()?;
+
+        let result = updates
+            .into_iter()
+            .filter(|update| Self::validate_update(update, &rules))
+            .map(|pages| pages[pages.len() / 2])
+            .sum::<usize>();
+
+        Ok(result.to_string())
+    }
+
+    fn solve_part_2(&self) -> Result<String> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::input::helpers::test_input;
+
+    #[test]
+    fn test_solve() {
+        let input = test_input(
+            r#"
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        "#,
+        );
+
+        let solver = Day5::new(Arc::new(Mutex::new(input)));
+        assert_eq!(solver.solve_part_1().unwrap(), "143");
+    }
+}
